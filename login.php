cde2fff16dd8d7595d3c5abde73dcda9fc968378
verameliana-alt <vera.meliana@mhs.politala.ac.id>
2025-09-02 15:03:59 +0800

    Update login.php

--- login.php
+++ login.php
@@ -49,7 +49,7 @@
 </head>
 <body>
   <div class="card">
-    <h1>Halo Selamat Datang 👋</h1>
+    <h1>Hii👋</h1>
     <form>
       <input type="email" placeholder="Email" required>
       <input type="password" placeholder="Password" required>
