721b3ebf2ee8505677c1cc9a4cc24ab2c687a55c
Rainn404 <gelang307@gmail.com>
2025-09-02 14:34:56 +0800

    saya membuat file baru Logout.php

---
+++ Logout.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+</head>
+<body>
+    <h1>Saya Mau Tidur</h1>
+</body>
+</html>
